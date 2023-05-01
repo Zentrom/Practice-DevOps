7ad9021e6b753b924627dfe4c05befe305661278
Zentrom <sokaturgen-17@hotmail.com>
2023-05-01 19:21:05 +0200

    v0.0.2-Package Manager
    -Added vcpkg in manifest mode.
    -Set up download of glm for vcpkg.

--- Practice-DevOps/main.cpp
+++ Practice-DevOps/main.cpp
@@ -1,3 +1,16 @@
+#include <glm\glm.hpp>
+
 int main() {
-	
+	//{
+	//	"name": "glm",
+	//		"default-features" : false,
+	//		"platform" : "!windows"
+	//},
+	//"overrides": [
+	//{
+	//	"name": "glm",
+	//		"version" : "0.9.9.8"
+	//}
+	//],
+	//	"builtin-baseline": "cb1301e9977b5f10b49f7988f68b808f7a558970"
 }
